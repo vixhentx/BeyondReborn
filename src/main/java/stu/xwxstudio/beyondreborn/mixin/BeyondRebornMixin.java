26a96d07c8f472ef3f46cac0eccf80f07a73ccbc
vixhentx <w1084349470@outlook.com>
2022-08-25 15:16:37 +0800

    The first frame

--- src/main/java/stu/xwxstudio/beyondreborn/mixin/BeyondRebornMixin.java
+++ src/main/java/stu/xwxstudio/beyondreborn/mixin/BeyondRebornMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package stu.xwxstudio.beyondreborn.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import stu.xwxstudio.beyondreborn.BeyondRebornMain;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class BeyondRebornMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		BeyondRebornMain.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
